b211127a78f2d263371ef28636d025a14e48a133
XleboMatb <103773997+XleboMatb@users.noreply.github.com>
2022-04-15 21:04:41 +0300

    Add files via upload

---
+++ GuidoBot123/GuidoBot123/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Discord;
+using Discord.Webhook;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Threading;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace GuidoBot123
+{
+    public class Class1 : BaseCommandModule
+    {
+       [Command("pog")]
+        public async Task Poggers(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("poggers").ConfigureAwait(false);
+        }
+        
+    }
+}
---
+++ GuidoBot123/GuidoBot123/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace GuidoBot123
+{
+    public class Class2 : BaseCommandModule
+    {
+        
+
+
+    }
+}
